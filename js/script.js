343ae6e645b86647780c3a45edb5557f2dd14356
AaronOngTW <aaronongtw@gmail.com>
2015-05-11 10:40:16 +1000

    added swipe left and right

--- js/script.js
+++ js/script.js
@@ -641,8 +641,8 @@ var gameGraphics = {
 
 
 
-$document..on("swiperight", dungeon.playerMoveRight)
-$document..on("swipeleft", dungeon.playerMoveLeft)
+$(document).on("swiperight", dungeon.playerMoveRight)
+$(document).on("swipeleft", dungeon.playerMoveLeft)
 $('#start').on("click", startGame)
 $(document).keypress(movementIdentity)
 $("#help").hover(help)
